ed0b131ae6e7c2a8b54c398caccd5dfe281fad4b
undefined <>
2018-07-12 03:35:04 +0800

    fix

--- zitoproduct.go
+++ zitoproduct.go
@@ -1,5 +1,8 @@
 package zitoproductface
 
+//Products Products
+var Products = make(map[string]ProductFace)
+
 //ProductFace 产品接口
 type ProductFace interface {
 	//GetPrefix 获得前置网址
@@ -7,4 +10,9 @@ type ProductFace interface {
 	GetTitle() string
 	//BindControllers 绑定beego 控制器
 	BindControllers()
+}
+
+//AddProduct AddProduct
+func AddProduct(prefix string, face ProductFace) {
+	Products[prefix] = face
 }
